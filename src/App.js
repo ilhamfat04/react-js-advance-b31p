a30e1efb67789ab99db69aa529f1c3413ca60b60
= <=>
2022-03-02 10:10:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/styles.css'
+
 // import components here
+import FormComponent from './components/Form';
 
 function App() {
   return (
     // code inside div
     <div>
-      
+      <FormComponent />
     </div>
   );
 }
